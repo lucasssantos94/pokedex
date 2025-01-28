0ab6f3c2d191f218978d9e58e7e8d997a5ad7bec
lucasssantos94 <lucas.ssantos94@outlook.com>
2025-01-27 23:56:52 -0300

    feat: animation made to open and close the modal

--- src/Components/ModalStats/index.jsx
+++ src/Components/ModalStats/index.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { motion, AnimatePresence } from "framer-motion";
 
 import iconClose from "../../assets/icon-close.svg";
 import pokeballDiveser from "../../assets/pokeball-diviser.svg";
-
 import iconWeight from "../../assets/icon-weight.svg";
 import iconHeight from "../../assets/icon-ruler.svg";
 
@@ -12,6 +12,7 @@ import BadgeType from "../BadgeType";
 
 const ModalStats = ({ modal, selectedPokemon }) => {
   const [progressValues, setProgressValues] = useState([]);
+  const [isExiting, setIsExiting] = useState(false);
 
   useEffect(() => {
     if (selectedPokemon) {
@@ -33,83 +34,109 @@ const ModalStats = ({ modal, selectedPokemon }) => {
     }
   }, [selectedPokemon]);
 
+  const handleClose = () => {
+    setIsExiting(true); // Ativa a animação de saída
+    setTimeout(() => {
+      modal(false); // Fecha o modal após a animação
+      setIsExiting(false); // Reseta o estado de saída
+    }, 300); // O tempo deve coincidir com o `duration` da animação
+  };
+
   return (
-    <div className={styles.modal} onClick={() => modal(false)}>
-      <div className={styles.container_modal}>
-        <img
-          src={iconClose}
-          alt="icone fechar modal"
-          className={styles.close_modal}
-          onClick={() => modal(false)}
-        />
+    <AnimatePresence>
+      {selectedPokemon && !isExiting && (
+        <motion.div
+          className={styles.modal}
+          onClick={handleClose}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          <motion.div
+            className={styles.container_modal}
+            onClick={(e) => e.stopPropagation()} // Impede o fechamento ao clicar dentro do modal
+            initial={{ scale: 0.8, opacity: 0 }}
+            animate={{ scale: 1, opacity: 1 }}
+            exit={{ scale: 0.4, opacity: 0 }}
+            transition={{ duration: 0.6 }}
+          >
+            <img
+              src={iconClose}
+              alt="icone fechar modal"
+              className={styles.close_modal}
+              onClick={handleClose}
+            />
 
-        <div className={styles.pokemon_info}>
-          {selectedPokemon && (
-            <div className={styles.info_pokemon}>
-              <img
-                src={selectedPokemon.sprites.other.home.front_default}
-                alt={selectedPokemon.name}
-                className={styles.image_pokemon}
-              />
-              <div className={styles.container_info}>
-                <p>
-                  #
-                  {selectedPokemon.id < 10
-                    ? `00${selectedPokemon.id}`
-                    : selectedPokemon.id < 100
-                      ? `0${selectedPokemon.id}`
-                      : selectedPokemon.id}
-                </p>
-                <h2>{selectedPokemon.name}</h2>
+            <div className={styles.pokemon_info}>
+              {selectedPokemon && (
+                <div className={styles.info_pokemon}>
+                  <img
+                    src={selectedPokemon.sprites.other.home.front_default}
+                    alt={selectedPokemon.name}
+                    className={styles.image_pokemon}
+                  />
+                  <div className={styles.container_info}>
+                    <p>
+                      #
+                      {selectedPokemon.id < 10
+                        ? `00${selectedPokemon.id}`
+                        : selectedPokemon.id < 100
+                          ? `0${selectedPokemon.id}`
+                          : selectedPokemon.id}
+                    </p>
+                    <h2>{selectedPokemon.name}</h2>
 
-                <div className={styles.types_pokemon}>
-                  {selectedPokemon.types.map((types, index) => (
-                    <BadgeType
-                      key={`${selectedPokemon.name}-${index}`}
-                      type={types.type.name}
-                    />
-                  ))}
+                    <div className={styles.types_pokemon}>
+                      {selectedPokemon.types.map((types, index) => (
+                        <BadgeType
+                          key={`${selectedPokemon.name}-${index}`}
+                          type={types.type.name}
+                        />
+                      ))}
+                    </div>
+
+                    <div className={styles.details_pokemon}>
+                      <span>
+                        <img src={iconWeight} alt="Weight" />
+                        {`${selectedPokemon.weight / 10} kg`}
+                      </span>
+                      <span>
+                        <img src={iconHeight} alt="Height" />
+                        {`${selectedPokemon.height / 10} m`}
+                      </span>
+                    </div>
+                  </div>
                 </div>
-
-                <div className={styles.details_pokemon}>
-                  <span>
-                    <img src={iconWeight} alt="Weight" />
-                    {`${selectedPokemon.weight / 10} kg`}
-                  </span>
-                  <span>
-                    <img src={iconHeight} alt="Height" />
-                    {`${selectedPokemon.height / 10} m`}
-                  </span>
-                </div>
-              </div>
+              )}
             </div>
-          )}
-        </div>
 
-        <div className={styles.divider}>
-          <img src={pokeballDiveser} alt="pokeball" />
-        </div>
+            <div className={styles.divider}>
+              <img src={pokeballDiveser} alt="pokeball" />
+            </div>
 
-        <div className={styles.pokemon_stats}>
-          <h2>Stats</h2>
+            <div className={styles.pokemon_stats}>
+              <h2>Stats</h2>
 
-          <table className={styles.table_stats}>
-            <tbody>
-              {selectedPokemon &&
-                selectedPokemon.stats.map((stats, index) => (
-                  <tr key={`${selectedPokemon.name}-${index}`}>
-                    <td>{stats.stat.name}</td>
-                    <td>{stats.base_stat}</td>
-                    <td>
-                      <progress value={progressValues[index]} max="100" />
-                    </td>
-                  </tr>
-                ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
+              <table className={styles.table_stats}>
+                <tbody>
+                  {selectedPokemon &&
+                    selectedPokemon.stats.map((stats, index) => (
+                      <tr key={`${selectedPokemon.name}-${index}`}>
+                        <td>{stats.stat.name}</td>
+                        <td>{stats.base_stat}</td>
+                        <td>
+                          <progress value={progressValues[index]} max="100" />
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </table>
+            </div>
+          </motion.div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
 
