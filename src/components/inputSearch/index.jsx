f3a84dc36655cba195671ac7eebd541f9cddaba0
lucasssantos94 <lucas.ssantos94@outlook.com>
2025-01-31 18:52:28 -0300

    feat: configured search by name or id

--- src/components/inputSearch/index.jsx
+++ src/components/inputSearch/index.jsx
@@ -1,9 +1,18 @@
+import { useNavigate } from "react-router-dom";
+
 import iconSearch from "../../assets/icon-search.svg";
 import styles from "./styles.module.scss";
 
 const InputSearch = () => {
+  const navigate = useNavigate();
+
+  const searchPokemon = (e) => {
+    e.preventDefault();
+    navigate(`/pokemon/${e.target[0].value}`);
+  };
+
   return (
-    <form className={styles.input_search}>
+    <form className={styles.input_search} onSubmit={searchPokemon}>
       <input type="text" placeholder="Pesquisar Pokémon" required />
       <button type="submit">
         <img src={iconSearch} alt="icone de lupa" />
--- src/pages/SearchByName/index.jsx
+++ src/pages/SearchByName/index.jsx
@@ -1,5 +1,53 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+import CardPokemon from "@components/CardPokemon";
+import ModalStats from "@components/ModalStats";
+
+import styles from "./styles.module.scss";
+
 const SearchByName = () => {
-  return <h1>SearchByName</h1>;
+  const { name } = useParams();
+  const [pokemon, setPokemon] = useState([]);
+  const [modal, setModal] = useState(false);
+  const [selectedPokemon, setSelectedPokemon] = useState();
+
+  const getData = async (name) => {
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BASE_URL}/pokemon/${name}`,
+      );
+      setPokemon(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getData(name);
+  }, [name]);
+
+  return (
+    <>
+      {pokemon?.name && (
+        <div>
+          {modal && (
+            <ModalStats modal={setModal} selectedPokemon={selectedPokemon} />
+          )}
+
+          <div className={styles.container_search}>
+            <CardPokemon
+              pokemon={pokemon}
+              modal={setModal}
+              selectedPokemon={setSelectedPokemon}
+            />
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default SearchByName;
