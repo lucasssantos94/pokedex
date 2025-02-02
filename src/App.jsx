4d9dc28055db59e2b5df3962d4683109daf3228a
lucasssantos94 <lucas.ssantos94@outlook.com>
2025-02-02 07:31:22 -0300

    style: added color variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import ScrollToTop from "react-scroll-to-top";
 import { Outlet } from "react-router-dom";
 
-import "./app.scss";
-
 import ContainerApp from "@components/ContainerApp";
 import Header from "@components/Header";
 import Navigation from "@components/Navigation";
